a62574bf9ff82694dc6953033a2f018d4004f77b
Yoann Cerda <tuxlinuxien@gmail.com>
2023-10-21 11:01:59 +0800

    Fix logger.

--- src/logger.rs
+++ src/logger.rs
@@ -2,6 +2,6 @@ use log::LevelFilter;
 
 pub fn init_logger(level: LevelFilter) {
     let mut builder = env_logger::builder();
-    builder.filter(Some("wirehole"), level);
+    builder.filter(Some("sns"), level);
     builder.init();
 }
